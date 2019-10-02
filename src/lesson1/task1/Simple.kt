63c023bc05e371ea97f9928f73148f27fa4e6709
YunoGasay <polina2001f@gmail.com>
2019-10-02 21:58:21 +0300

    .

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -48,7 +48,7 @@ fun quadraticRootProduct(a: Double, b: Double, c: Double): Double {
 /**
  * Пример главной функции
  */
-fun main(args:Array<String>) {
+fun main(args: Array<String>) {
     val x1x2 = quadraticRootProduct(1.0, 13.0, 42.0)
     println("Root product: $x1x2")
 }
@@ -68,7 +68,8 @@ fun seconds(hours: Int, minutes: Int, seconds: Int): Int = 3600 * hours + 60 * m
  * Определить длину того же отрезка в метрах (в данном случае 18.98).
  * 1 сажень = 3 аршина = 48 вершков, 1 вершок = 4.445 см.
  */
-fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = (sagenes * 48 + 16 * arshins + vershoks) * 4.445 / 100
+fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = (sagenes * 48 + 16 * arshins + vershoks) *
+        4.445 / 100
 
 /**
  * Тривиальная
@@ -76,8 +77,8 @@ fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = (sagenes
  * Пользователь задает угол в градусах, минутах и секундах (например, 36 градусов 14 минут 35 секунд).
  * Вывести значение того же угла в радианах (например, 0.63256).
  */
-fun angleInRadian(deg: Int, min: Int, sec: Int): Double = 3600 * deg + 60 * min + sec / 2 * 3.14
-
+fun angleInRadian(deg: Int, min: Int, sec: Int): Double =
+    deg * PI / 180 + min * PI / (180 * 60) + sec * PI / (180 * 60 * 60)
 
 /**
  * Тривиальная
@@ -85,7 +86,9 @@ fun angleInRadian(deg: Int, min: Int, sec: Int): Double = 3600 * deg + 60 * min 
  * Найти длину отрезка, соединяющего точки на плоскости с координатами (x1, y1) и (x2, y2).
  * Например, расстояние между (3, 0) и (0, 4) равно 5
  */
-fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1))
+fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) *
+            (y2 - y1)
+)
 
 /**
  * Простая
@@ -93,7 +96,7 @@ fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = sqrt((
  * Пользователь задает целое число, большее 100 (например, 3801).
  * Определить третью цифру справа в этом числе (в данном случае 8).
  */
-fun thirdDigit(number: Int): Int = number - number / 100
+fun thirdDigit(number: Int): Int = (number - number / 1000 * 1000) / 100
 
 /**
  * Простая
@@ -102,7 +105,8 @@ fun thirdDigit(number: Int): Int = number - number / 100
  * прибыл на станцию назначения в h2 часов m2 минут того же дня (например в 13:01).
  * Определите время поезда в пути в минутах (в данном случае 216).
  */
-fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int = 60 * (hoursArrive - hoursDepart) + (minutesArrive - minutesDepart)
+fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int = 60 * (hoursArrive -
+        hoursDepart) + (minutesArrive - minutesDepart)
 
 /**
  * Простая
@@ -111,7 +115,10 @@ fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minute
  * Сколько денег будет на счету через 3 года (с учётом сложных процентов)?
  * Например, 100 рублей под 10% годовых превратятся в 133.1 рубля
  */
-fun accountInThreeYears(initial: Int, percent: Int): Double = ((((initial + initial * percent) + initial * percent) + initial * percent)) * 0.01
+fun accountInThreeYears(initial: Int, percent: Int): Double =
+    ((initial + initial * percent * 0.01) + ((initial + initial * percent * 0.01) * percent * 0.01)) +
+            (((initial + initial * percent * 0.01) + ((initial + initial * percent * 0.01) * percent * 0.01)) *
+                    percent * 0.01)
 
 /**
  * Простая
@@ -119,4 +126,5 @@ fun accountInThreeYears(initial: Int, percent: Int): Double = ((((initial + init
  * Пользователь задает целое трехзначное число (например, 478).
  * Необходимо вывести число, полученное из заданного перестановкой цифр в обратном порядке (например, 874).
  */
-fun numberRevert(number: Int): Int = number / 100 + number / 10 / 10 * 10 + number / 10 / 10 / 10 * 100
+fun numberRevert(number: Int): Int = number / 100 + ((number - number / 100 * 100) - (number - number / 10 * 10)) +
+        (number - number / 10 * 10) * 100
